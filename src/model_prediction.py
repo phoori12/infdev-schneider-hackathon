9d0c7686fcf7906cc295c982a316723691db0e4d
Woraphon Choaywong <phoori12@gmail.com>
2023-11-20 00:54:07 +0100

    not working prediction

--- src/model_prediction.py
+++ src/model_prediction.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import argparse
 import torch
+from torch.utils.data import DataLoader, Dataset
 from model_training import MulticlassClassification as mcc_model
+from model_training import ClassifierDataset
+import numpy as np
 
 def load_data(file_path):
     df = pd.read_csv(file_path)
@@ -14,19 +17,44 @@ def load_model(model_path):
     return model
 
 def make_predictions(df, model):
-
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    with torch.no_grad():  # Disable gradient computation during inference
+    df = df.iloc[: , 1:11]
+    df = df.astype(float)
+    print(df.values)
+    # x_val = np.array(df.iloc[: , 1:10])
+    # y_val = np.array(df.iloc[: , -1])
 
-        predictions = model(torch.tensor(df.values))
-    return predictions
+    #dataset = ClassifierDataset(torch.from_numpy(x_val).float(), torch.from_numpy(y_val).long())
+
+    model.to(device)
+    # model.eval()
+    y_pred_list = []
+
+    with torch.no_grad():  # Disable gradient computation during inference
+        predictions = model(torch.tensor(df))
+    print(predictions.shape)
+
+    # for X_batch, _ in test_loader:
+    #     X_batch = X_batch.to(device)
+    #     y_test_pred = model(X_batch)
+    #     _, y_pred_tags = torch.max(y_test_pred, dim = 1)
+    #     y_pred_list.append(y_pred_tags.cpu().numpy())
+
+    # y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+    # # y_pred_list = np.array(y_pred_list).flatten().tolist()
+    # print(y_pred_list)
+    
+    return y_pred_list
+
 
 def save_predictions(predictions, predictions_file):
     predictions_list = predictions.tolist()
-    json_path = predictions_file
-    with open(json_path, 'w') as json_file:
-        json.dump(predictions_list, json_file)
-    pass
+    
+    # print(predictions_list)
+    # json_path = predictions_file
+    # with open(json_path, 'w') as json_file:
+    #     json.dump(predictions_list, json_file)
+    # pass
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Prediction script for Energy Forecasting Hackathon')
@@ -51,12 +79,12 @@ def parse_arguments():
     return parser.parse_args()
 
 def main(input_file, model_file, output_file):
-    df = load_data("/home/main/Hackathon/infdev-schneider-hackathon/data/test_final.csv")
-    model = load_model("/home/main/Hackathon/infdev-schneider-hackathon/models/model.pt")
+    df = load_data("../data/test_final.csv")
+    model = load_model("../models/model.pt")
     model.eval()
     print(model)
     predictions = make_predictions(df, model)
-    save_predictions(predictions, "/home/main/Hackathon/infdev-schneider-hackathon/predictions/predictions.json")
+    save_predictions(predictions, "../predictions/predictions.json")
 
 if __name__ == "__main__":
     args = parse_arguments()
